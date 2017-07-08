c7c4d5e46e675411127e087f38531ae76da2b5c9
alete89 <maildealete@gmail.com>
2017-07-08 15:21:59 -0300

    rework del form principal

---
+++ Forms/Principal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Controles2016.Forms
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Lecturas_Enter(object sender, EventArgs e)
+        {
+            ((TextBox)sender).Text = "";
+            ((TextBox)sender).ForeColor = Color.Black;
+        }
+
+        private void Lecturas_Leave(object sender, EventArgs e)
+        {
+            ((TextBox)sender).Text = ((TextBox)sender).Text.Replace(".", ",");
+        }
+    }
+}
